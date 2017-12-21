edb47d1a0a04cecbfa629cd03fb70b7fa54112b4
Caomr <634780246@qq.com>
2017-12-21 21:45:25 +0800

    优化

--- src/main/resources/DB/table.sql
+++ src/main/resources/DB/table.sql
@@ -12,4 +12,16 @@ CREATE TABLE `cmr_goods_info` (
   `update_time` datetime DEFAULT NULL COMMENT '跟新时间',
   PRIMARY KEY (`id`),
   KEY `idx_goodsid_goodsstatus` (`goods_id`,`goods_status`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `xxl_api_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL COMMENT '账号',
+  `password` varchar(50) NOT NULL COMMENT '密码',
+  `type` tinyint(4) NOT NULL DEFAULT '0' COMMENT '用户类型：0-普通用户、1-超级管理员',
+  `realname` varchar(50) DEFAULT NULL COMMENT '真实姓名',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+insert into xxl_api_user(username,password,type,realname) values('admin','123456',1,'超级管理员');
